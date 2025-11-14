08620d94c31a0b4d4e1708ee1071414d5945f99c
mohammedazeem1307-source <mohammedazeem1307@gmail.com>
2025-11-14 15:41:11 +0530

    Add files via upload

---
+++ Program to Compute Student Grade based on total marks and display result.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int mark1, mark2, mark3, total;
+    float average, percentage;
+    char grade;
+
+    printf("Enter marks for Subject 1: ");
+    scanf("%d", &mark1);
+    printf("Enter marks for Subject 2: ");
+    scanf("%d", &mark2);
+    printf("Enter marks for Subject 3: ");
+    scanf("%d", &mark3);
+
+    total = mark1 + mark2 + mark3;
+    average = total / 3.0;
+    percentage = (total / 300.0) * 100;
+
+    if (mark1 < 50 || mark2 < 50 || mark3 < 50) {
+        printf("\nResult: FAIL\n");
+        grade = 'F';
+    } else {
+        printf("\nResult: PASS\n");
+
+        if (percentage >= 90)
+            grade = 'A';
+        else if (percentage >= 75)
+            grade = 'B';
+        else if (percentage >= 60)
+            grade = 'C';
+        else
+            grade = 'D';  // 50 to 59
+    }
+
+    printf("Marks: %d, %d, %d\n", mark1, mark2, mark3);
+    printf("Total Marks: %d out of 300\n", total);
+    printf("Average Marks: %.2f\n", average);
+    printf("Percentage: %.2f%%\n", percentage);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
